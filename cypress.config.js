c190877b5ff1cd17a5bc905ee564341369806c8f
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-08-07 17:02:12 +0530

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,5 @@
 // cypress.config.js
-module.exports = {
-    e2e: {
-      setupNodeEvents(on, config) {
-        // implement node event listeners here
-      },
-      baseUrl: 'http://localhost:8080', // Update with your app's base URL
-      // other Cypress configuration options
-    },
-    // you can also add other configurations here
-  };
-  
+
+export const e2e = {
+  supportFile: 'cypress/support/e2e.js',
+};
